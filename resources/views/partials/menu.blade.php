1f40dc43c8a2afb51ef125384b37823384b518d6
Erich Briceño <erichbriceno@gmail.com>
2016-05-29 22:43:37 -0400

    Implement Component in memu

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,20 +1,19 @@
 <ul class="nav navbar-nav">
-    <li class="active"><a href="http://tacmail">Inicio</a></li>
+    <li @if(Route::is('mails.control')) class="active" @endif><a href="http://tacmail">Inicio</a></li>
 
-    <li class="dropdown">
+    <li @if(Route::is('mails.querySuitcases')) class="active" @endif class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
-            Acciones <span class="caret"></span>
+            Acciones<span class="caret"></span>
             <ul class="dropdown-menu" role="menu">
-                <li><a href="http://tacmail/querySuitcases">Valija</a></li>
-                <li><a href="#">Carga</a></li>
-                <li><a href="#">Remitir</a></li>
+                <li><a href="{{ route('mails.querySuitcases')}}">Valija</a></li>
+                <li><a href="{{ route('mails.upload')}}">Carga</a></li>
             </ul>
         </a>
     </li>
 
     <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
-            Seguimiento <span class="caret"></span>
+            Seguimiento<span class="caret"></span>
             <ul class="dropdown-menu" role="menu">
                 <li><a href="#">General</a></li>
                 <li><a href="#">Valijas</a></li>
